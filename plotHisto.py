190da3cf160dcb2eab4108cf699e95a086aa43b4
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2014-08-21 18:48:28 -0300

    Fixed fitted gaussian.

--- plotHisto.py
+++ plotHisto.py
@@ -5,22 +5,21 @@
 import numpy as np
 import matplotlib as mpl
 from matplotlib import pyplot as plt
-#from scipy.optimize import curve_fit
-from astropy.modeling import models, fitting
+from scipy.optimize import curve_fit
+#from astropy.modeling import models, fitting
 
 mpl.rcParams['font.family'] = 'sans-serif'
 #versionSuffix = 'v20_q036.d13c'
 versionSuffix = 'v20_q043.d14a'
+outputImgSuffix = 'pdf'
 
-fit = False
-#fit = True
+#fit = False
+fit = True
 
-#EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-# # Define model function to be used to fit to the data above:
-# def gauss(x, *p):
-#     A, mu, sigma = p
-#     return A * np.exp(-(x - mu) ** 2 / (2. * sigma ** 2))
-#EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+# Define model function to be used to fit to the data above:
+def gauss(x, *p):
+    A, mu, sigma = p
+    return A * np.exp(-(x - mu) ** 2 / (2. * sigma ** 2))
 
 def plotFitHisto(binCenter, histo, title, fileName, fit = False):
     f = plt.figure()
@@ -28,24 +27,19 @@ def plotFitHisto(binCenter, histo, title, fileName, fit = False):
     f.set_size_inches(2 * 3.45, 6)
     
     ax = plt.gca()
-    ax.bar(binCenter, histo, width = 0.08, edgecolor = 'black', color = 'lightgrey')
+    ax.bar(binCenter, histo, width = 0.05, edgecolor = 'black', color = 'lightgrey')
     ax.set_title(title)
+    
+    if fit:
+        coeff, var_matrix = curve_fit(gauss, binCenter, histo, p0 = [1000., 0., 1.])
+        y = gauss(binCenter, *coeff)
+        ax.plot(binCenter, y, 'b-')
+        ax.text(0.96, 0.96, '$A=%.2f$\n$x_0=%.2f$\n$\sigma=%.2f$' % (coeff[0], coeff[1], np.abs(coeff[2])),
+                fontsize = 12, transform = ax.transAxes, va = 'top', ha = 'right',
+                bbox = dict(boxstyle = 'round', facecolor = 'wheat', alpha = 0.))
+
     ax.set_xlabel(r'$(O_{\lambda k}\ -\ M_{\lambda k}) / \epsilon_{\lambda k}$')
     ax.set_ylabel(r'Number of pixels')
-
-    if fit:
-        # Fit data
-        g_init = models.Gaussian1D(amplitude=1., mean=0, stddev=1.)
-        fit_g = fitting.LevMarLSQFitter()
-        g = fit_g(g_init, binCenter, histo)
-        ax.plot(binCenter, g(binCenter))
-        #EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
-        # coeff, var_matrix = curve_fit(gauss, binCenter, histo, p0 = [1., 0., 1.])
-        # ax.plot(binCenter, gauss(binCenter, *coeff), 'b-')
-        # ax.text(0.96, 0.96, '$A=%.2f$\n$x_0=%.2f$\n$\sigma=%.2f$' % (coeff[0], coeff[1], coeff[2]),
-        #         fontsize = 12, transform = ax.transAxes, va = 'top', ha = 'right',
-        #         bbox = dict(boxstyle = 'round', facecolor = 'wheat', alpha = 0.))
-        #EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 
     f.tight_layout()
     f.savefig(fileName)
@@ -56,6 +50,6 @@ if __name__ == '__main__':
         for rad in ['Galaxy', 'Nucleus', 'Bulge', 'Disc']:
             fileName = 'SpecResidStats4DR2_HistsRSU.%s.%s' % (wei, rad)
             binCenter, histoR, histoS, histoU = np.loadtxt(fileName, unpack = True)
-            plotFitHisto(binCenter, histoR, '%s %s' % (rad, versionSuffix), 'histoR.%s,%s.%s.png' % (versionSuffix, wei, rad), fit = fit)
-            plotFitHisto(binCenter, histoS, '%s %s' % (rad, versionSuffix), 'histoS.%s.%s.%s.png' % (versionSuffix, wei, rad), fit = fit)
-            plotFitHisto(binCenter, histoU, '%s %s' % (rad, versionSuffix), 'histoU.%s.%s.%s.png' % (versionSuffix, wei, rad), fit = fit)
+            plotFitHisto(binCenter, histoR, '%s %s' % (rad, versionSuffix), 'histoR.%s.%s.%s.%s' % (versionSuffix, wei, rad, outputImgSuffix), fit = fit)
+            plotFitHisto(binCenter, histoS, '%s %s' % (rad, versionSuffix), 'histoS.%s.%s.%s.%s' % (versionSuffix, wei, rad, outputImgSuffix), fit = fit)
+            plotFitHisto(binCenter, histoU, '%s %s' % (rad, versionSuffix), 'histoU.%s.%s.%s.%s' % (versionSuffix, wei, rad, outputImgSuffix), fit = fit)
